9ee5ebbe37ec4a9288928ce530501a32e29e6676
d.voyanets <diana.voyanets@gmail.com>
2018-02-21 21:56:57 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.store = [];
+      this.compareFunction = (left, right) => left - right;
   }
 
   add(element) {
-    // your implementation
+      this.store.push(element);
   }
 
   at(index) {
-    // your implementation
+      return this.store[index];
   }
 
   get length() {
-    // your implementation
+      return this.store.length;
   }
 
   toArray() {
-    // your implementation
+      return this.store.slice(); // ha! 
   }
 
   sort(indices) {
-    // your implementation
+      var indicesSave = indices.slice().sort();
+
+      var sortedIndices = indices
+          .map(value => this.store[value])
+          .sort(this.compareFunction);
+
+      this.store.forEach((value, i) => {
+          var findIdx = indicesSave.indexOf(i);
+
+          if (findIdx !== -1) {
+              this.store[i] = sortedIndices[findIdx]; 
+          }
+      });
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      if (typeof compareFunction !== 'function') {
+          return;
+      }
+
+      this.compareFunction = compareFunction;
   }
 }
 
